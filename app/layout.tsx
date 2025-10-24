3696757d307bad075c910fdaca9602b0298e6a6a
jay <michaeldjay@gmail.com>
2025-10-24 16:02:55 +0100

    remove suspense from top layout; remove other routes

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import {Suspense} from 'react';
+import { Suspense } from "react";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,7 +28,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <Suspense fallback={<div>Loading Page...</div>}>{children}</Suspense>
+        {children}
       </body>
     </html>
   );
